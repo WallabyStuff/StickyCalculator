9f38b8347c6e0dd47b6b8345ced9cd06f23e8091
SunneyG <avocado34.131@gmail.com>
2022-03-16 16:40:08 +0900

    Initial Commit

---
+++ StickyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyCalculator
+//
+//  Created by 이승기 on 2022/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
