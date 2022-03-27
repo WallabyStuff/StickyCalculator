4435c735f5a5fd6bccba2b631bd2059969a3129c
SunneyG <avocado34.131@gmail.com>
2022-03-27 17:13:39 +0900

    Feat: Prepare views
    Add all views based on storyboard. All views are responding to all size classes.
    
    Add 'NanumSquareRoundOff' font family which are used to keypad buttons and textLabels to project file
    
    Add reactor files for Reactor pattern. Each viewController has a corresponding reactor file.
    
    Add icons which are used to iconKeypadButtons
    
    Merge or Add colors for dark and light appearances
    
    Refs: #3

---
+++ StickyCalculator/Sources/Views/HistoryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryTableViewCell.swift
+//  StickyCalculator
+//
+//  Created by 이승기 on 2022/03/24.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var expressionLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ StickyCalculator/Sources/Views/TextKeypadButton.swift
@@ -0,0 +1,20 @@
+//
+//  TextKeypadButton.swift
+//  StickyCalculator
+//
+//  Created by 이승기 on 2022/03/24.
+//
+
+import UIKit
+
+@IBDesignable
+class TextKeypadButton: KeypadButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
